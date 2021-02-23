9974f26d6898920810233a3a602ee3a796a01bfc
Alex Amistad <alex@A-5CG7362WBZ.erni2.ch>
2021-02-23 12:28:43 +0800

    update

--- src/hocs/withChat.js
+++ src/hocs/withChat.js
@@ -1,32 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { compose } from 'redux';
 
 const withChat = (ChildComponent) => compose((props) => {
   const { auth } = props;
 
-  useEffect(() => {
-    if (!auth.user) {
-      return;
-    }
-    /* eslint-disable */
-    window.fbAsyncInit = function () {
-        FB.init({
-            xfbml: true,
-            version: 'v9.0',
-        });
-    };
+  if (!auth.user) {
+    return <ChildComponent {...props} />;
+  }
 
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    /* eslint-enable */
-  }, [auth.user]);
-
-  return <ChildComponent {...props} />;
+  return (
+    <>
+      <ChildComponent {...props} />
+      <div id="fb-root" />
+      <div className="fb-customerchat" attribution="setup_tool" page_id="100394824911272" theme_color="#ff7e29" />
+    </>
+  );
 });
 
 export default withChat;
