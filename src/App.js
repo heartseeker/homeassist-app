b2f847dd2c35fa38d6aa197578c7669cc9bded50
Alex Amistad <alex@A-5CG7362WBZ.erni2.ch>
2021-02-23 09:18:27 +0800

    update chat

--- src/App.js
+++ src/App.js
@@ -51,6 +51,27 @@ const App = ({
     checkUser();
   }, []);
 
+  useEffect(() => {
+    if (auth.user) {
+      /* eslint-disable */
+      window.fbAsyncInit = function () {
+        FB.init({
+          xfbml: true,
+          version: 'v9.0',
+        });
+      };
+
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+      /* eslint-enable */
+    }
+  }, [auth.user]);
+
   return (
     <Router>
       <Switch>
@@ -63,6 +84,8 @@ const App = ({
         <Route path="/home-loan-qualfying" component={HomeLoanQualifying} />
         <Route path="/" exact component={auth.user ? PropertySearch : Signin} />
       </Switch>
+      <div id="fb-root" />
+      <div className="fb-customerchat" attribution="setup_tool" page_id="100394824911272" theme_color="#ff7e29" />
     </Router>
   );
 };
