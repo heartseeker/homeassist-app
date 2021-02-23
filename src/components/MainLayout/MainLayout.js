477c6ef1dfcefd2b20a968958f9df5bdd7863fe2
Alex Amistad <alex@A-5CG7362WBZ.erni2.ch>
2021-02-23 08:53:02 +0800

    add facebook chat

--- src/components/MainLayout/MainLayout.js
+++ src/components/MainLayout/MainLayout.js
@@ -52,7 +52,23 @@ const MainLayout = ({
   };
 
   useEffect(() => {
+    /* eslint-disable */
     window.addEventListener('scroll', handleScroll);
+    window.fbAsyncInit = function () {
+      FB.init({
+        xfbml: true,
+        version: 'v9.0',
+      });
+    };
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+     /* eslint-enable */
   }, []);
 
   if (!auth.user) {
@@ -69,6 +85,8 @@ const MainLayout = ({
           {children}
         </div>
       </main>
+      <div id="fb-root" />
+      <div className="fb-customerchat" attribution="setup_tool" page_id="100394824911272" theme_color="#ff7e29" />
     </div>
   );
 };
