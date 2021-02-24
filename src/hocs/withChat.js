2857fd5e5cb668912c2fd2cf3316901829310f9f
Alex Amistad <alex@A-5CG7362WBZ.erni2.ch>
2021-02-24 10:38:51 +0800

    update

--- src/hocs/withChat.js
+++ src/hocs/withChat.js
@@ -6,14 +6,14 @@ const withChat = (ChildComponent) => compose((props) => {
 
   useEffect(() => {
     /* eslint-disable */
-    window.fbAsyncInit = function () {
+    window.fbAsyncInit = function() {
       FB.init({
-          xfbml: true,
-          version: 'v9.0',
+        xfbml            : true,
+        version          : 'v10.0'
       });
     };
 
-    (function (d, s, id) {
+    (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
@@ -27,7 +27,7 @@ const withChat = (ChildComponent) => compose((props) => {
     <>
       <ChildComponent {...props} />
       <div id="fb-root" style={{ visibility: !auth.user ? 'hidden' : 'visible' }} />
-      <div className="fb-customerchat" attribution="setup_tool" page_id="100394824911272" theme_color="#ff7e29" />
+      <div className="fb-customerchat" attribution="setup_tool" page_id="447166058770713" theme_color="#ff7e29" />
     </>
   );
 });
